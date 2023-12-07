f23a30d6a9dfd4ec94334fd2459059fa9facb3a8
Shilo724 <134378269+Shilo724@users.noreply.github.com>
2023-12-07 03:12:49 +0000

    bootstap error

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+    # => app/views/static_pages/contact.html.erb
+  end
+end
