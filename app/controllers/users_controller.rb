b2f15ae6e6fec9fcb6203021a8e6f13075c61e4b
Shilo724 <134378269+Shilo724@users.noreply.github.com>
2023-12-23 15:51:13 +0000

    seventh_class_3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
+  # GET /users/id
   def show
     #@user = User.first
     @user = User.find(params[:id])
 
-    foo = "foobar"
-    hoge = "hogehoge"
-    debugger
+    # foo = "foobar"
+    # hoge = "hogehoge"
+    # debugger
   end
 
+  # GET /users/new.html.erb
   def new
     # => app/views/users/new.html.erb
   end
