0808dcfb7d9a515a1cf68a1340ea3f12897edd53
Shilo724 <134378269+Shilo724@users.noreply.github.com>
2023-12-25 14:58:12 +0000

    class_7.3_updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,20 @@ class UsersController < ApplicationController
   end
 
   #POST /users
-  def Create
-    
+  def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
+
+  private
+
+    #strong parameters
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
